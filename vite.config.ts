74b086ab2f84a0954e9fa611c8d1e64eb4cf6597
liftoach <petivan@technologysolutionsgeorgia.ge>
2025-05-31 00:30:11 +0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Kursach-front/',  // <- Укажи здесь точное имя репозитория
+  base: '/',  // <- Укажи здесь точное имя репозитория
   plugins: [react()],
   build: {
     outDir: 'build',
